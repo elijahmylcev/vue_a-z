6ce5ed8ba921e2aba74b51b6a91c1f29aa2eb1c1
Elijah Mylcev <elijahmylcev@gmail.com>
2022-01-24 17:55:37 +0300

    fixed problems

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
-	<div>
-		<form></form>
-		<div class="post" v-for="post in posts">
+	<div class="app">
+		<form>
+			<h3>Создание поста</h3>
+			<input
+				v-bind:value="title"
+				class="input"
+				type="text"
+				placeholder="Название поста"
+			/>
+			<input
+				v-bind:value="body"
+				class="input"
+				type="text"
+				placeholder="Описание поста"
+			/>
+
+			<button class="btn" @click="createPost">Создать</button>
+		</form>
+		<div class="post" v-for="post in posts" :key="post.id">
 			<div><strong>Название</strong> {{ post.title }}</div>
 			<div><strong>Описание</strong> {{ post.body }}</div>
 		</div>
@@ -29,7 +45,12 @@ export default {
 					body: 'JS был представлен в 1995 году',
 				},
 			],
+			title,
+			body,
 		};
+	},
+	methods: {
+		createPost() {},
 	},
 };
 </script>
@@ -42,8 +63,34 @@ export default {
 	margin-top: 15px;
 }
 
+.app {
+	padding: 15px;
+}
+
+form {
+	display: flex;
+	flex-direction: column;
+}
+
 .post {
 	padding: 15px;
 	border: 2px solid green;
+}
+
+.input {
+	display: block;
+	margin: 10px auto 0 auto;
+	width: 100%;
+	border: 1px solid teal;
+	padding: 10px 15px;
+}
+
+.btn {
+	align-self: flex-end;
+	margin-top: 10px;
+	padding: 10px 15px;
+	background: none;
+	color: teal;
+	border: 1px solid teal;
 }
 </style>
